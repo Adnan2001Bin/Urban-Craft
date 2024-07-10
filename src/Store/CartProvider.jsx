34b958d231a123b95133e151728255fbca601ad0
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-07-10 18:21:42 +0600

    cart button

--- src/Store/CartProvider.jsx
+++ src/Store/CartProvider.jsx
@@ -110,9 +110,48 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const exitingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[exitingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[exitingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE") {
+    const exitingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[exitingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[exitingCartItemIndex] = updatedItem;
+    }
 
     return {
       items: updatedItems,
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,93 +1,84 @@
-import "./Cart.css"
-import ModalCart from "./ModalCart"
-// import { Link } from "react-router-dom"
-// import CartContext from "../../../Store/cart-context"
-// import { useContext } from "react"
-// import Cart_Card from "./Cart-Card"
-
+import "./Cart.css";
+import ModalCart from "./ModalCart";
+import { Link } from "react-router-dom";
+import CartContext from "../../Store/cart-context";
+import { useContext } from "react";
+import Cart_Card from "./Cart-Card";
 
 function Cart(props) {
-  // const cartCtx = useContext(CartContext)
-  // const totalAmount = `৳${cartCtx.totalAmount}`
-  // const hasItems = cartCtx.items.length > 0;
-  
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `৳${cartCtx.totalAmount}`;
+  const hasItems = cartCtx.items.length > 0;
 
-  // const cartItemRemoveHandler = (id) => {
-  //   cartCtx.removeItem(id)
-  //  };
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  // const cartItemAddHandler = (item) => {
-  //   cartCtx.addItem({...item , amount:1})
-  // };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
-  
+  const handleClick = () => {
+    props.onShowCheckOut();
+    props.onClose();
+  };
 
-
-  // const handleClick = () => {
-  //   props.onShowCheckOut()
-  //   props.onClose()
-  // }
-
-
-
-  // const cartItems = (
-  //   <ul className="cart-items bg-white">
-  //     {cartCtx.items.map((item) => (
-  //       <Cart_Card
-  //         key={item.id}
-  //         name={item.name}
-  //         img={item.img}
-  //         amount={item.amount}
-  //         price={item.price}
-  //         onRemove={cartItemRemoveHandler.bind(null, item.id)}
-  //         onAdd={cartItemAddHandler.bind(null, item)}
-
-  //       />
-  //     )
-  //     )}
-  //   </ul>
-  // )
-
-
-
+  const cartItems = (
+    <ul className="cart-items bg-white">
+      {cartCtx.items.map((item) => (
+        <Cart_Card
+          key={item.id}
+          name={item.name}
+          img={item.img}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
 
   return (
     <ModalCart onClose={props.onClose}>
-
-      {/* <div className="Cart"> */}
-
+      <div className="Cart">
         <div className="mt-5 text-xl font-bold">
           <h1>Your Cart</h1>
         </div>
 
+        {hasItems && <div>{cartItems}</div>}
 
-        {/* {hasItems && <div>
-          {cartItems}
-        </div>}
-
-
-        {!hasItems &&
+        {!hasItems && (
           <div className="emptyCart">
-
             <img className="w-4/5" src="assets\Untitled design.png" alt="" />
             <h1 className="text-xl font-bold">Your Cart is feeling lonely</h1>
 
-
-            <Link to='/'>
-              <button onClick={props.onClose} className="bg-black text-white w-40 h-10 rounded-xl mt-5">Start Shopping</button>
+            <Link to="/">
+              <button
+                onClick={props.onClose}
+                className="bg-black text-white w-40 h-10 rounded-xl mt-5"
+              >
+                Start Shopping
+              </button>
             </Link>
-          </div>}
+          </div>
+        )}
       </div>
 
-      {hasItems && <div className="totalAmount flex items-center justify-between w-full bg-gray-100">
-        <p className="text-2xl font-semibold pl-4">{totalAmount}</p>
+      {hasItems && (
+        <div className="totalAmount flex items-center justify-between w-full bg-gray-100">
+          <p className="text-2xl font-semibold pl-4">{totalAmount}</p>
 
-        <button  onClick={handleClick} className={`w-44 text-white bg-black border border-black hover:bg-gray-600 rounded-xl h-14 text-center `}>Confirm Order</button>
-      </div>} */}
-
+          <button
+            onClick={handleClick}
+            className={`w-44 text-white bg-black border border-black hover:bg-gray-600 rounded-xl h-14 text-center `}
+          >
+            Confirm Order
+          </button>
+        </div>
+      )}
     </ModalCart>
-
-  )
+  );
 }
 
-export default Cart
+export default Cart;
